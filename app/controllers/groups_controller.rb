f8615ee79da96929246e7719305f5174eb55eeac
kensaku33 <picow.zoe33@gmail.com>
2019-08-29 20:53:20 +0900

    edit side-bar for group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,14 +1,32 @@
 class GroupsController < ApplicationController
-end
 
-def new
-end
+  def index
+  end
 
-def create
-end
-
-def edit
-end
+  def new
+    @group = Group.new
+    @group.users << current_user
+  end
   
-def update
+  def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to root_path, notice: 'グループを作成しました'
+    else
+      render :new
+    end
+  end
+  
+  def edit
+  end
+    
+  def update
+  end
+  
+  private
+    def group_params
+      params.require(:group).permit(:name, { :user_ids => [] })
+    end
+
+
 end
