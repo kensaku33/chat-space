8891be585c6498f3870f31d20cefef1fd463f0d0
kensaku33 <picow.zoe33@gmail.com>
2019-08-30 17:46:46 +0900

    edit message sending, fixed view

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-
   devise_for :users
-  # root to: 'messages#index'
   root to: 'groups#index'
   resources :users, only: [:edit, :update]
   resources :groups, only: [:new, :create, :edit, :update] do
