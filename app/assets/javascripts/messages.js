f125c27fd0723038220d1ada2febdf7a18d648ec
kensaku33 <picow.zoe33@gmail.com>
2019-09-11 16:45:18 +0900

    edit

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -53,33 +53,35 @@ $(document).on("turbolinks:load", function() {
   })
 
   var reloadMessages = function() {
+    if (window.location.href.match(/\/groups\/\d+\/messages/)){
     //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
-    last_message_id = $('.message:last').data('message-id');
-    $.ajax({
-      //ルーティングで設定した通りのURLを指定
-      url: 'api/messages',
-      //ルーティングで設定した通りhttpメソッドをgetに指定
-      type: 'get',
-      dataType: 'json',
-      //dataオプションでリクエストに値を含める
-      data: {id: last_message_id}
-    })
-    .done(function(messages) {
-      //追加するHTMLの入れ物を作る
-      var insertHTML = '';
-      //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
-      messages.forEach(function(message){
-      //メッセージが入ったHTMLを取得
-      insertHTML = buildHTML(message);
-      console.log(insertHTML)
-      //メッセージを追加
-      $('.chat-main__messages').append(insertHTML);
-      $('.chat-main__messages').animate({ scrollTop: $('.chat-main__messages')[0].scrollHeight});
+      last_message_id = $('.message:last').data('message-id');
+      $.ajax({
+        //ルーティングで設定した通りのURLを指定
+        url: 'api/messages',
+        //ルーティングで設定した通りhttpメソッドをgetに指定
+        type: 'get',
+        dataType: 'json',
+        //dataオプションでリクエストに値を含める
+        data: {id: last_message_id}
+      })
+      .done(function(messages) {
+        //追加するHTMLの入れ物を作る
+        var insertHTML = '';
+        //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+        messages.forEach(function(message){
+        //メッセージが入ったHTMLを取得
+        insertHTML = buildHTML(message);
+        console.log(insertHTML)
+        //メッセージを追加
+        $('.chat-main__messages').append(insertHTML);
+        $('.chat-main__messages').animate({ scrollTop: $('.chat-main__messages')[0].scrollHeight});
+        });
+      })
+      .fail(function() {
+        console.log('error');
       });
-    })
-    .fail(function() {
-      console.log('error');
-    });
+    };
   };
   setInterval(reloadMessages, 5000);
 })
